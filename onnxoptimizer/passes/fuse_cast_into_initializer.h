dad59995595d9bc82356802e6409449cf029b413
Johannes Frohnhofen <jf@daedalean.ai>
2021-06-16 11:04:12 +0200

    Use enum for data types + cast elements to correct target type

--- onnxoptimizer/passes/fuse_cast_into_initializer.h
+++ onnxoptimizer/passes/fuse_cast_into_initializer.h
@@ -21,11 +21,30 @@ namespace optimization {
 
 namespace {
 
-template <typename A, typename B>
+enum DataType {
+  FLOAT = 1,
+  UINT8 = 2,
+  INT8 = 3,
+  UINT16 = 4,
+  INT16 = 5,
+  INT32 = 6,
+  INT64 = 7,
+  STRING = 8,       // not supported
+  BOOL = 9,         // not supported
+  FLOAT16 = 10,     // not supported
+  DOUBLE = 11,
+  UINT32 = 12,
+  UINT64 = 13,
+  COMPLEX64 = 14,   // not supported
+  COMPLEX128 = 15,  // not supported
+  BFLOAT16 = 16     // not supported
+};
+
+template <typename T, typename A, typename B>
 void copyData(std::vector<A>& dst, const B* src, const size_t len) {
     dst.reserve(len);
     for (size_t i=0; i<len; ++i) {
-        dst.push_back(src[i]);
+        dst.push_back(static_cast<T>(src[i]));
     }
 }
 
@@ -64,120 +83,218 @@ struct FuseCastIntoInitializer final : public PredicateBasedPass {
     size_t num_elements = oldTensor->sizes().empty() ? 1 :num_elements = oldTensor->size_from_dim(0); 
 
     switch (newTensor.elem_type()) {
-    case 1:
+    case DataType::FLOAT:
       switch (oldTensor->elem_type()) {
-        case 2:
-        case 3:
-        case 4:
-        case 5:
-        case 6:
-          copyData(newTensor.floats(), oldTensor->data<int32_t>(), num_elements);
+        case DataType::UINT8:
+        case DataType::INT8:
+        case DataType::UINT16:
+        case DataType::INT16:
+        case DataType::INT32:
+          copyData<float>(newTensor.floats(), oldTensor->data<int32_t>(), num_elements);
           break;
-        case 7:
-          copyData(newTensor.floats(), oldTensor->data<int64_t>(), num_elements);
+        case DataType::INT64:
+          copyData<float>(newTensor.floats(), oldTensor->data<int64_t>(), num_elements);
           break;
-        case 11:
-          copyData(newTensor.floats(), oldTensor->data<double>(), num_elements);
+        case DataType::DOUBLE:
+          copyData<float>(newTensor.floats(), oldTensor->data<double>(), num_elements);
           break;
-        case 12:
-        case 13:
-          copyData(newTensor.floats(), oldTensor->data<uint64_t>(), num_elements);
+        case DataType::UINT32:
+        case DataType::UINT64:
+          copyData<float>(newTensor.floats(), oldTensor->data<uint64_t>(), num_elements);
           break;
         default:
           return false;
         }
         break;
     
-    case 2:
-    case 3:
-    case 4:
-    case 5:
-    case 6:
+    case DataType::UINT8:
       switch (oldTensor->elem_type()) {
-        case 1:
-          copyData(newTensor.int32s(), oldTensor->data<float>(), num_elements);
+        case DataType::FLOAT:
+          copyData<uint8_t>(newTensor.int32s(), oldTensor->data<float>(), num_elements);
           break;
-        case 7:
-          copyData(newTensor.int32s(), oldTensor->data<int64_t>(), num_elements);
+        case DataType::INT64:
+          copyData<uint8_t>(newTensor.int32s(), oldTensor->data<int64_t>(), num_elements);
           break;
-        case 11:
-          copyData(newTensor.int32s(), oldTensor->data<double>(), num_elements);
+        case DataType::DOUBLE:
+          copyData<uint8_t>(newTensor.int32s(), oldTensor->data<double>(), num_elements);
           break;
-        case 12:
-        case 13:
-          copyData(newTensor.int32s(), oldTensor->data<uint64_t>(), num_elements);
+        case DataType::UINT32:
+        case DataType::UINT64:
+          copyData<uint8_t>(newTensor.int32s(), oldTensor->data<uint64_t>(), num_elements);
           break;
         default:
           return false;
         }
         break;
 
-    case 7:
+    case DataType::INT8:
       switch (oldTensor->elem_type()) {
-        case 1:
-          copyData(newTensor.int64s(), oldTensor->data<float>(), num_elements);
+        case DataType::FLOAT:
+          copyData<int8_t>(newTensor.int32s(), oldTensor->data<float>(), num_elements);
           break;
-        case 2:
-        case 3:
-        case 4:
-        case 5:
-        case 6:
-          copyData(newTensor.int64s(), oldTensor->data<int32_t>(), num_elements);
+        case DataType::INT64:
+          copyData<int8_t>(newTensor.int32s(), oldTensor->data<int64_t>(), num_elements);
           break;
-        case 11:
-          copyData(newTensor.int64s(), oldTensor->data<double>(), num_elements);
+        case DataType::DOUBLE:
+          copyData<int8_t>(newTensor.int32s(), oldTensor->data<double>(), num_elements);
           break;
-        case 12:
-        case 13:
-          copyData(newTensor.int64s(), oldTensor->data<uint64_t>(), num_elements);
+        case DataType::UINT32:
+        case DataType::UINT64:
+          copyData<int8_t>(newTensor.int32s(), oldTensor->data<uint64_t>(), num_elements);
           break;
         default:
           return false;
         }
         break;
 
-    case 11:
+    case DataType::UINT16:
       switch (oldTensor->elem_type()) {
-        case 1:
-          copyData(newTensor.doubles(), oldTensor->data<float>(), num_elements);
+        case DataType::FLOAT:
+          copyData<uint16_t>(newTensor.int32s(), oldTensor->data<float>(), num_elements);
           break;
-        case 2:
-        case 3:
-        case 4:
-        case 5:
-        case 6:
-          copyData(newTensor.doubles(), oldTensor->data<int32_t>(), num_elements);
+        case DataType::INT64:
+          copyData<uint16_t>(newTensor.int32s(), oldTensor->data<int64_t>(), num_elements);
           break;
-        case 7:
-          copyData(newTensor.doubles(), oldTensor->data<int64_t>(), num_elements);
+        case DataType::DOUBLE:
+          copyData<uint16_t>(newTensor.int32s(), oldTensor->data<double>(), num_elements);
           break;
-        case 12:
-        case 13:
-          copyData(newTensor.doubles(), oldTensor->data<uint64_t>(), num_elements);
+        case DataType::UINT32:
+        case DataType::UINT64:
+          copyData<uint16_t>(newTensor.int32s(), oldTensor->data<uint64_t>(), num_elements);
           break;
         default:
           return false;
         }
         break;
 
-    case 12:
-    case 13:
+    case DataType::INT16:
       switch (oldTensor->elem_type()) {
-        case 1:
-          copyData(newTensor.uint64s(), oldTensor->data<float>(), num_elements);
+        case DataType::FLOAT:
+          copyData<int16_t>(newTensor.int32s(), oldTensor->data<float>(), num_elements);
           break;
-        case 2:
-        case 3:
-        case 4:
-        case 5:
-        case 6:
-          copyData(newTensor.uint64s(), oldTensor->data<int32_t>(), num_elements);
+        case DataType::INT64:
+          copyData<int16_t>(newTensor.int32s(), oldTensor->data<int64_t>(), num_elements);
           break;
-        case 7:
-          copyData(newTensor.uint64s(), oldTensor->data<int64_t>(), num_elements);
+        case DataType::DOUBLE:
+          copyData<int16_t>(newTensor.int32s(), oldTensor->data<double>(), num_elements);
           break;
-        case 11:
-          copyData(newTensor.uint64s(), oldTensor->data<double>(), num_elements);
+        case DataType::UINT32:
+        case DataType::UINT64:
+          copyData<int16_t>(newTensor.int32s(), oldTensor->data<uint64_t>(), num_elements);
+          break;
+        default:
+          return false;
+        }
+        break;
+
+    case DataType::INT32:
+      switch (oldTensor->elem_type()) {
+        case DataType::FLOAT:
+          copyData<int32_t>(newTensor.int32s(), oldTensor->data<float>(), num_elements);
+          break;
+        case DataType::INT64:
+          copyData<int32_t>(newTensor.int32s(), oldTensor->data<int64_t>(), num_elements);
+          break;
+        case DataType::DOUBLE:
+          copyData<int32_t>(newTensor.int32s(), oldTensor->data<double>(), num_elements);
+          break;
+        case DataType::UINT32:
+        case DataType::UINT64:
+          copyData<int32_t>(newTensor.int32s(), oldTensor->data<uint64_t>(), num_elements);
+          break;
+        default:
+          return false;
+        }
+        break;
+
+    case DataType::INT64:
+      switch (oldTensor->elem_type()) {
+        case DataType::FLOAT:
+          copyData<int64_t>(newTensor.int64s(), oldTensor->data<float>(), num_elements);
+          break;
+        case DataType::UINT8:
+        case DataType::INT8:
+        case DataType::UINT16:
+        case DataType::INT16:
+        case DataType::INT32:
+          copyData<int64_t>(newTensor.int64s(), oldTensor->data<int32_t>(), num_elements);
+          break;
+        case DataType::DOUBLE:
+          copyData<int64_t>(newTensor.int64s(), oldTensor->data<double>(), num_elements);
+          break;
+        case DataType::UINT32:
+        case DataType::UINT64:
+          copyData<int64_t>(newTensor.int64s(), oldTensor->data<uint64_t>(), num_elements);
+          break;
+        default:
+          return false;
+        }
+        break;
+
+    case DataType::DOUBLE:
+      switch (oldTensor->elem_type()) {
+        case DataType::FLOAT:
+          copyData<double>(newTensor.doubles(), oldTensor->data<float>(), num_elements);
+          break;
+        case DataType::UINT8:
+        case DataType::INT8:
+        case DataType::UINT16:
+        case DataType::INT16:
+        case DataType::INT32:
+          copyData<double>(newTensor.doubles(), oldTensor->data<int32_t>(), num_elements);
+          break;
+        case DataType::INT64:
+          copyData<double>(newTensor.doubles(), oldTensor->data<int64_t>(), num_elements);
+          break;
+        case DataType::UINT32:
+        case DataType::UINT64:
+          copyData<double>(newTensor.doubles(), oldTensor->data<uint64_t>(), num_elements);
+          break;
+        default:
+          return false;
+        }
+        break;
+
+    case DataType::UINT32:
+      switch (oldTensor->elem_type()) {
+        case DataType::FLOAT:
+          copyData<uint32_t>(newTensor.uint64s(), oldTensor->data<float>(), num_elements);
+          break;
+        case DataType::UINT8:
+        case DataType::INT8:
+        case DataType::UINT16:
+        case DataType::INT16:
+        case DataType::INT32:
+          copyData<uint32_t>(newTensor.uint64s(), oldTensor->data<int32_t>(), num_elements);
+          break;
+        case DataType::INT64:
+          copyData<uint32_t>(newTensor.uint64s(), oldTensor->data<int64_t>(), num_elements);
+          break;
+        case DataType::DOUBLE:
+          copyData<uint32_t>(newTensor.uint64s(), oldTensor->data<double>(), num_elements);
+          break;
+        default:
+          return false;
+        }
+        break;
+
+    case DataType::UINT64:
+      switch (oldTensor->elem_type()) {
+        case DataType::FLOAT:
+          copyData<uint64_t>(newTensor.uint64s(), oldTensor->data<float>(), num_elements);
+          break;
+        case DataType::UINT8:
+        case DataType::INT8:
+        case DataType::UINT16:
+        case DataType::INT16:
+        case DataType::INT32:
+          copyData<uint64_t>(newTensor.uint64s(), oldTensor->data<int32_t>(), num_elements);
+          break;
+        case DataType::INT64:
+          copyData<uint64_t>(newTensor.uint64s(), oldTensor->data<int64_t>(), num_elements);
+          break;
+        case DataType::DOUBLE:
+          copyData<uint64_t>(newTensor.uint64s(), oldTensor->data<double>(), num_elements);
           break;
         default:
           return false;
@@ -189,9 +306,7 @@ struct FuseCastIntoInitializer final : public PredicateBasedPass {
     }
     
     Value* newValue = graph.addInitializerAndInput(newTensor);
-    const bool replacing_success =
-        tryReplacingAllUsesWith(node->output(), newValue);
-    if (!replacing_success) {
+    if (!tryReplacingAllUsesWith(node->output(), newValue)) {
       return false;
     }
     destroy_current = NodeDestroyType::DestroyOne;
