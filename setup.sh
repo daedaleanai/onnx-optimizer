da5d4c489ec1fca7d1e5781523afddbc94de1b28
Johannes Frohnhofen <jf@daedalean.ai>
2022-08-10 11:59:22 +0200

    symlink protobuf include dir and library

--- setup.sh
+++ setup.sh
@@ -2,5 +2,8 @@
 cd $(dirname "$0")
 git submodule update --init --recursive
 mkdir -p build && cd build
-cmake -DONNX_ML=OFF ..
+PROTOBUF_INCL=$(cmake -DONNX_ML=OFF .. | grep 'Protobuf includes'| tr -s ' ' | cut -d' ' -f5)
+PROTOBUF_LIBS=$(cmake -DONNX_ML=OFF .. | grep 'Protobuf libraries'| tr -s ' ' | cut -d' ' -f5)
+ln -s $PROTOBUF_INCL protobuf
+ln -s $PROTOBUF_LIBS .
 make -j8
